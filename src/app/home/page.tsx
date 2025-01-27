2fff6fd6c357283e93fd47cf7ade70cb811222cb
Shimomura Tatsuro <kmgeong1127@gmail.com>
2025-01-28 07:54:20 +0900

    2025/01/28 api追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,21 @@
 import "./page.css"
 import CommonHeader from "../common/commonHeader";
 import PictureList from "./PictureList";
+import React, { useState, useEffect } from 'react';
+
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8080/api');
+      const json = await response.json();
+      setData(json);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <header>
@@ -11,7 +24,7 @@ export default function Home() {
       </header>
       <body>
         <div className="selected_group">
-          <h1 className="text_selected_group">注目のサークル</h1>
+          <h1 className="text_selected_group">注目のサークル{data}</h1>
           <div className='slider'>
             <PictureList />
           </div>
